9b0dde0391c63f4bd34dffbdf11c80284f01c89c
srivarkor <srivardhanakorada@digiframe.org>
2022-01-20 15:13:28 +0530

    Debug

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from http import HTTPStatus
 from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from authlib.integrations.starlette_client import OAuth, OAuthError
@@ -66,6 +67,33 @@ async def all_places():
         }
     }
 
-@app.post("/googlesignin")
+@app.post("/googlesignin",status_code=status.HTTP_200_OK)
 async def google_signin(request:Request):
-    return request.json()
+   return {
+        "places":{
+            "warangalfort":{
+                "placename":"warangalfort",
+                "placelat":"28.0235",
+                "placelong":"52.214",
+                "distfromstay":"20"
+            },
+            "bhadrakalitemple":{
+                "placename":"bhadrakalitemple",
+                "placelat":"25.075",
+                "placelong":"42.48",
+                "distfromstay":"10"
+            },
+            "wonderla":{
+                "placename":"wonderla",
+                "placelat":"74.07",
+                "placelong":"14.87",
+                "distfromstay":"50"
+            },
+            "s2cinemas":{
+                "placename":"s2cinemas",
+                "placelat":"51.87",
+                "placelong":"84.87",
+                "distfromstay":"5"
+            }
+        }
+    }
