066b09a0c34668d95d3b08b81ee4d7bb41af36eb
srivarkor <srivardhanakorada@digiframe.org>
2022-01-20 15:48:20 +0530

    Change

--- app/main.py
+++ app/main.py
@@ -69,32 +69,7 @@ async def all_places():
 
 @app.post("/googlesignin",status_code=status.HTTP_200_OK)
 async def google_signin(request:Request):
-    print(request.body())
+    s = request.body()
     return {
-        "places":{
-            "warangalfort":{
-                "placename":"warangalfort",
-                "placelat":"28.0235",
-                "placelong":"52.214",
-                "distfromstay":"20"
-            },
-            "bhadrakalitemple":{
-                "placename":"bhadrakalitemple",
-                "placelat":"25.075",
-                "placelong":"42.48",
-                "distfromstay":"10"
-            },
-            "wonderla":{
-                "placename":"wonderla",
-                "placelat":"74.07",
-                "placelong":"14.87",
-                "distfromstay":"50"
-            },
-            "s2cinemas":{
-                "placename":"s2cinemas",
-                "placelat":"51.87",
-                "placelong":"84.87",
-                "distfromstay":"5"
-            }
-        }
+        "Received" : s.__str__
     }
