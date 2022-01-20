c5aa8892247471dda81fbb2c07f8272e7fc74547
srivarkor <srivardhanakorada@digiframe.org>
2022-01-20 13:45:46 +0530

    Modifying CORS

--- app/main.py
+++ app/main.py
@@ -7,9 +7,14 @@ from starlette.config import Config
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8000",
+    "http://localhost",
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
